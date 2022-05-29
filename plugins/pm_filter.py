11aec380d97633a1013a59b1e92ce373b35ca6cd
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-05-29 09:20:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -802,7 +802,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
-        if not START_IMAGE_URL:
+        if not random.choice(PICS):
             await message.reply(
                 script.START_TXT.format(
                     (message.from_user.mention if 
